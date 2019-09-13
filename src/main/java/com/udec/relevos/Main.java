68e08937ef221d342ebbed43f592e7a91c96a3a7
Tatiana Moreno Ramirez <tmoreno980813@gmail.com>
2019-09-13 18:08:40 -0500

    Se agrega la referencia para pruebas y el main

---
+++ src/main/java/com/udec/relevos/Main.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.udec.relevos;
+
+/**
+ *  Clase que ejecuta el proyecto
+ * @author Tatiana Moreno, Andres Chila
+ */
+public class Main {
+    public static void main(String[] args){
+     
+    }
+}
