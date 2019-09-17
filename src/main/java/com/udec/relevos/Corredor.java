6ba49a79c2d91a6f12845fd24fb24309ae455992
Tatiana Moreno Ramirez <tmoreno980813@gmail.com>
2019-09-16 21:06:55 -0500

    Se agregan atributos a la clase del corredor

--- src/main/java/com/udec/relevos/Corredor.java
+++ src/main/java/com/udec/relevos/Corredor.java
@@ -6,9 +6,35 @@
 package com.udec.relevos;
 
 /**
- *
+ *  Clase con los atributos de un corredor 
  * @author tmore
  */
 public class Corredor {
+    //Variable que guarda la cantidad de pasos que da el corredor este va de 0 a 3 
+    private int pasos;
+    //Variable que guarda el nombre del corredor
+    private String nombre;
+    //Constructor de la clase
+    public Corredor(int pasos, String nombre) {
+        this.pasos = pasos;
+        this.nombre = nombre;
+    }
+
+    public int getPasos() {
+        return pasos;
+    }
+
+    public void setPasos(int pasos) {
+        this.pasos = pasos;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+    
     
 }
